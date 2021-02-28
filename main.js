598d96b12929c5d92087fadd335cbd341288dcdb
cesar juarez <cesar.juarez@codeimmersives.com>
2021-02-28 01:55:50 -0800

    my-code

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 /******************************
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
+function fizzy(num){
+  if (num % 3 === 0 && num % 5 === 0){
+    return "FizzBuzz";}
+    else if ( num % 3 === 0){
+      return "Fizz";
+    }else if ( num % 5 === 0){
+      return "Buzz";
+    }else{
+      return num;
+    }
+  }
 
+// another way to write the above code
+function fizzyAlt2 (num){
+  let result = '';
+if (num % 3 === 0){
+  result = result + "fizz";
+}
+if ( num % 5 === 0){
+  result = result + "Buzz";
+}
 
+}
 
 
 
